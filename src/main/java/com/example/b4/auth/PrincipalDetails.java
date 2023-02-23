1ae6d932184e2f90e9f2394c208d2814e67773e2
seongwon-ha <djfqks22@naver.com>
2023-02-23 17:04:56 +0900

    merge update 3

--- src/main/java/com/example/b4/auth/PrincipalDetails.java
+++ src/main/java/com/example/b4/auth/PrincipalDetails.java
@@ -7,6 +7,7 @@ import lombok.ToString;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collection;
--- src/main/java/com/example/b4/repository/UserRepository.java
+++ src/main/java/com/example/b4/repository/UserRepository.java
@@ -3,10 +3,13 @@ package com.example.b4.repository;
 import com.example.b4.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findById(Long userId);
     User findByUsername(String username);
+    Optional<User> findByLoginId(String loginId);
+    Optional<User> findByEmail(String email);
 }
